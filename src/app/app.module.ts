51a127597453619d9087ca9fa2039155500ad77a
ferge22 <rytisspitrys@gmail.com>
2018-07-16 11:59:19 +0300

    priject setup and instalations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    IngredientComponent
   ],
   imports: [
     BrowserModule
