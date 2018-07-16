901177ac29246ca17b5361806867aa268a00cce8
ferge22 <rytisspitrys@gmail.com>
2018-07-16 14:02:24 +0300

    replanning architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './ingredient/ingredient.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RecipesComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
-    IngredientComponent
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
+
   ],
   imports: [
     BrowserModule
